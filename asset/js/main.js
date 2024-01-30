fc1b3abf07c4aaf8ecd7fc4c3927df9ea6152eac
Prasun Paudel <prasunpaudel2001@gmail.com>
2024-01-30 13:10:08 +0545

    validation edited

--- asset/js/main.js
+++ asset/js/main.js
@@ -14,15 +14,15 @@ function showContent(targetId) {
 }
 //form validation in contact section
 function validateForm(){
-	var name= document.forms["contactForm"]["fullName"].value;
+	var name= document.forms["contactForm"][""]["fullName"].value;
 	var emailAddress= document.forms["contactForm"]["emailAddress"].value;
 	var subject= document.forms["contactForm"]["subject"].value;
 	var message= document.forms["contactForm"]["message"].value;
-	if(name == ""  || emailAddress == "" || subject == "" || message ==""){//it the field is empty this message is s
+	if(name == ""  || emailAddress == "" || subject == "" || message ==""){//if the field is empty this message is alerted
 		alert("Empty fields found. Please fill the form.");
 	}
 	else{
-		alert("Thank you for your feedback.")//it any of the field is not empty this message is shown
+		alert("Thank you for your feedback.")//if any of the field is not empty this message is alerted
 	}
 	
 }
